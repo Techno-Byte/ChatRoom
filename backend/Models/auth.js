f287bc25f958daf24ac13332c3cbd27bf49f8c3f
sunil <sunilssaharan@gmail.com>
2018-01-07 22:36:44 +0530

    made changes

--- backend/Models/auth.js
+++ backend/Models/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '527681834237720', // your App ID
         'clientSecret'    : '917dd08df1fd84777ea6c5af94844f50', // your App Secret
-        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
+        'callbackURL'     : 'https://gkwzaavozy.localtunnel.me/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
 
     },
@@ -20,7 +20,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'         : '412467755457-ma18eqdheti42ns6818q57k6nrk1bajb.apps.googleusercontent.com',
         'clientSecret'     : '9gtH-OdrX3PmDTZjLo1dEadI',
-        'callbackURL'      : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'      : 'https://gkwzaavozy.localtunnel.me/auth/google/callback'
     }
 
 };
